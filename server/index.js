0ba973274a6e58cb2c1ad7c1613ce26b535c1bee
Derek Wheelden <derek.wheelden@gmail.com>
2023-02-01 18:34:11 -0500

    Add some junk

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const Arg = require('arg');
 const Glue = require('@hapi/glue');
 const Manifest = require('./manifest');
 
-exports.deployment = async () => {
+exports.deployment = async (args) => {
 
     const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
@@ -12,7 +13,7 @@ exports.deployment = async () => {
 
     const { centralService } = server.services();
 
-    await centralService.start();
+    await centralService.start(args);
 
     // This keeps the process from exiting
     setInterval(() => { /* no-op */ }, 5000);
@@ -22,7 +23,14 @@ exports.deployment = async () => {
 
 if (require.main === module) {
 
-    exports.deployment();
+    const args = Arg({
+        '--autoStart': Boolean,
+        '-a': '--autoStart'
+    });
+
+    exports.deployment({
+        autoStart: !!args['--autoStart']
+    });
 
     process.on('unhandledRejection', (err) => {
 
